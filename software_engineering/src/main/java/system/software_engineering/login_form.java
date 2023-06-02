39c61eb34cfc94502e870a73984a311956f8e667
John Laurence Burgos <johnlaurenceburgos@gmail.com>
2023-06-02 10:13:47 +0800

    update na di na ako mahal

--- software_engineering/src/main/java/system/software_engineering/login_form.java
+++ software_engineering/src/main/java/system/software_engineering/login_form.java
@@ -124,7 +124,7 @@ public class login_form extends javax.swing.JFrame {
                     show(false);
                     
                 } else {
-                    JOptionPane.showMessageDialog(frame,"Invalid Username or Password!");
+                    JOptionPane.showMessageDialog(frame,"di na ako mahal");
                 }
             }
              
